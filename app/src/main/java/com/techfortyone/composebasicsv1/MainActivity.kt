1c2088e1705773e913fd4a787efdc93ee1d74b65
Saikrishna41 <saai.krrishna@gmail.com>
2024-03-19 17:13:12 +0530

    state hoisting

---
+++ app/src/main/java/com/techfortyone/composebasicsv1/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.techfortyone.composebasicsv1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.techfortyone.composebasicsv1.ui.theme.ComposeBasicsv1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeBasicsv1Theme {
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        }
+        else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun Greetings(modifier: Modifier = Modifier, names: List<String> = listOf("Compose", "World")) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(modifier = modifier, name = name)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding = if (expanded) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .wrapContentHeight()
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(
+                    text = "Hello"
+                )
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { expanded = !expanded },
+                modifier = Modifier.padding(start = 20.dp)
+            ) {
+                Text(if (expanded) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+data class Employee(
+    val name: String
+)
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview(employee: Employee = Employee("sai")) {
+    ComposeBasicsv1Theme {
+        Greetings()
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked : () -> Unit, modifier: Modifier = Modifier) {
+    //this state should be hoisted
+    var shouldShowOnboarding by remember {
+        mutableStateOf(true)
+    }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the basics of codelab")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text(text = "continue")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun OnboardingPreview() {
+    ComposeBasicsv1Theme {
+        OnboardingScreen(onContinueClicked = {})
+    }
+}
